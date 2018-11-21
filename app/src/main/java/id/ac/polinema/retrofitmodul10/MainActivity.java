33f03ad02704d0a11a9f8199b73ecf013c6a651a
TAHTA REZA R <kimtahta@gmail.com>
2018-11-21 09:46:17 +0700

    RetrofitModul10

---
+++ app/src/main/java/id/ac/polinema/retrofitmodul10/MainActivity.java
@@ -0,0 +1,54 @@
+package id.ac.polinema.retrofitmodul10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import id.ac.polinema.retrofitmodul10.Adapters.MoviesAdapter;
+import id.ac.polinema.retrofitmodul10.Models.Movie;
+import id.ac.polinema.retrofitmodul10.Models.MovieResponse;
+import id.ac.polinema.retrofitmodul10.Rest.ApiClient;
+import id.ac.polinema.retrofitmodul10.Rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private final static String API_KEY = "76d6ce7feb5a3c6c01073708f8fba06d";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (API_KEY.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY first from themoviedb.org", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        Call<MovieResponse> call = apiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse>call, Response<MovieResponse> response) {
+                int statusCode = response.code();
+                List<Movie> movies = response.body().getResults();
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+            }
+            @Override
+            public void onFailure(Call<MovieResponse>call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/id/ac/polinema/retrofitmodul10/Models/Movie.java
@@ -0,0 +1,54 @@
+package id.ac.polinema.retrofitmodul10.Models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Movie {
+    @SerializedName("overview")
+    private String overview;
+    @SerializedName("release_date")
+    private String releaseDate;
+    @SerializedName("id")
+    private Integer id;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("vote_average")
+    private Double voteAverage;
+    public Movie(String overview, String releaseDate, Integer id,
+                 String title, Double voteAverage) {
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.id = id;
+        this.title = title;
+        this.voteAverage = voteAverage;
+    }
+    public String getOverview() {
+        return overview;
+    }
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+}
